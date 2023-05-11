aff0acf6f80c1bd5e3929705bc89a8438f1f9fe3
EscribanoPablo <pescribano@edu.tecnocampus.cat>
2023-05-11 02:06:41 +0200

    Trampa 1
    Roca cae en la cabezaaa mueres

---
+++ Assets/V1_PrototypeSans/Scripts/Traps.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Traps : MonoBehaviour
+{
+    Rigidbody2D _rb;
+
+    //Collision2D _rockCollider;
+
+    [SerializeField]
+    PlayerController Player;
+
+    private void Awake()
+    {
+        //_rockCollider = GetComponentInParent<Collision2D>();
+        _rb = GetComponentInParent<Rigidbody2D>();
+       _rb.isKinematic = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform == Player.transform)
+        {
+            Debug.Log("detected");
+            _rb.isKinematic = false;
+        }
+    }
+}
