b074cd89b8a6e5b99a04c2dfd9fc95080248f3e9
EscribanoPablo <pescribano@edu.tecnocampus.cat>
2023-05-12 16:12:14 +0200

    aaaaaa

--- Assets/V1_PrototypeSans/Scripts/RockTrap.cs
+++ Assets/V1_PrototypeSans/Scripts/RockTrap.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Traps : MonoBehaviour
+public class RockTrap : MonoBehaviour
 {
     Rigidbody2D _rb;
 
