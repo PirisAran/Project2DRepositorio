e19b55da8e1dc168ab131b750fbcc0fe84a37d94
MarcSans6 <marcsanscormenzana@gmail.com>
2023-05-15 20:41:19 +0200

    checkpoint

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -1,24 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace TecnocampusProjectII
 {
 	public class LevelController : MonoBehaviour
 	{
+        [SerializeField] GameObject _player;
 		public string m_NextLevel;
+        
+        [SerializeField] private Transform _spawnPoint;
+        private void Awake()
+        {
+            _spawnPoint.position = Vector2.zero;
+            _player.transform.position = _spawnPoint.position;
+        }
 
-		private void Update()
+        private void Update()
         {
             if (ConditionNextLevel())
             {
                 Debug.Log("load next level " + m_NextLevel);
                 SceneManager.LoadSceneAsync(m_NextLevel);
             }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                RespawnPlayer();
+            }
         }
 
-        private static bool ConditionNextLevel()
+        private bool ConditionNextLevel()
         {
             return Input.GetKeyDown(KeyCode.N);
+        }
+
+        public void RespawnPlayer()
+        {
+            _player.transform.position = _spawnPoint.position;
+        }
+
+        public void SetSpawnpoint(Vector3 pos)
+        {
+            _spawnPoint.position = pos;
         }
     }
 }
---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TecnocampusProjectII;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField] Transform _spawnPoint;
+    [SerializeField] KeyCode _interactKey = KeyCode.E;
+    [SerializeField] LevelController _levelController;
+
+    bool _activated = false;
+
+    SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Thrower thrower = collision.GetComponent<Thrower>();
+        if (thrower == null)
+            return;
+
+        if (_activated)
+            return;
+        
+        if (Input.GetKey(_interactKey) && thrower.HasFire)
+        {
+            ActivateCheckpoint();
+            ActivateAnim();
+        }
+    }
+
+    private void ActivateAnim()
+    {
+        _spriteRenderer.color = Color.green;
+    }
+
+    private void ActivateCheckpoint()
+    {
+        Debug.Log("checkpoint activated");
+        _levelController.SetSpawnpoint(_spawnPoint.position);
+        _activated = true;
+    }
+}
