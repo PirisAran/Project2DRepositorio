fc117a2d113f0d0baec61173f53de539ca0c6e15
MarcSans6 <marcsanscormenzana@gmail.com>
2023-05-18 02:25:06 +0200

    door anim done

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,6 +8,7 @@ public class Door : MonoBehaviour
     [SerializeField] DoorSwitch _switch;
     [SerializeField] float _openSpeed;
     [SerializeField] Transform _openPosition;
+    [SerializeField] Animator _anim;
     bool _isOpening = false;
 
     private void OnEnable()
@@ -19,7 +20,6 @@ public class Door : MonoBehaviour
     {
         _switch.OnSwitchActivated -= OnSwitchActivated;
     }
-
     void Start()
     {
         
@@ -42,12 +42,13 @@ public class Door : MonoBehaviour
 
     private void OnSwitchActivated()
     {
-        StartOpenDoor();
+        _anim.SetBool("activated", true);
     }
 
-    private void StartOpenDoor()
+    public void StartOpenDoor()
     {
         Debug.Log("Door opening start");
         _isOpening = true;
+        _anim.SetBool("activated", true);
     }
 }
