17ba6de407b883a20a9ae3ceacf1d2569fb86e3b
EscribanoPablo <pescribano@edu.tecnocampus.cat>
2023-05-19 17:53:35 +0200

    Charcos, basals

---
+++ Assets/Scripts/Water2/WaterController/WaterSpring.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using TecnocampusProjectII;
+
+public class WaterSpring : MonoBehaviour
+{
+    [SerializeField] Transform _player;
+    Rigidbody2D _playerRB;
+    public float velocity = 0;
+    public float force = 0;
+    // current height
+    public float height = 0f;
+    // normal height
+    private float target_height = 0f;
+    public Transform springTransform;
+    [SerializeField]
+    private static SpriteShapeController spriteShapeController = null;
+    private int waveIndex = 0;
+    private List<WaterSpring> springs = new();
+    private float resistance = 40f;
+
+    private void Start()
+    {
+        _player = GameLogic.GetGameLogic().GetGameController().m_Player.transform;
+        _playerRB = _player.GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(SpriteShapeController ssc) { 
+
+        var index = transform.GetSiblingIndex();
+        waveIndex = index+1;
+        
+        spriteShapeController = ssc;
+        velocity = 0;
+        height = transform.localPosition.y;
+        target_height = transform.localPosition.y;
+    }
+    // with dampening
+    // adding the dampening to the force
+    public void WaveSpringUpdate(float springStiffness, float dampening) { 
+        height = transform.localPosition.y;
+        // maximum extension
+        var x = height - target_height;
+        var loss = -dampening * velocity;
+
+        force = - springStiffness * x + loss;
+        velocity += force;
+        var y = transform.localPosition.y;  
+        transform.localPosition = new Vector3(transform.localPosition.x, y+velocity, transform.localPosition.z);
+  
+    }
+    public void WavePointUpdate() { 
+        if (spriteShapeController != null) {
+            Spline waterSpline = spriteShapeController.spline;
+            Vector3 wavePosition = waterSpline.GetPosition(waveIndex);
+            waterSpline.SetPosition(waveIndex, new Vector3(wavePosition.x, transform.localPosition.y, wavePosition.z));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.transform == _player.transform)
+        {
+            Debug.Log("Object with RB");
+
+            Rigidbody2D rb = _playerRB;
+            var speed = rb.velocity;
+            velocity += speed.y / resistance;
+        }
+        //if (collision.gameObject.tag.Equals("FallingObject"))
+        //{
+        //    FallingObject fallingObject = collision.gameObject.GetComponent<FallingObject>();
+        //    Rigidbody2D rb = fallingObject.rigidbody2D;
+        //    var speed = rb.velocity;
+
+        //    velocity += speed.y / resistance;
+        //}
+    }
+
+}
