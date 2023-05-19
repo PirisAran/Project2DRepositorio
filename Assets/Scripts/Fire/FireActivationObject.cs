d9ddaad86b307edbfecb88a1ee15120667b8231e
MarcSans6 <marcsanscormenzana@gmail.com>
2023-05-19 16:05:13 +0200

    door switch better

--- Assets/Scripts/Fire/FireActivationObject.cs
+++ Assets/Scripts/Fire/FireActivationObject.cs
@@ -7,33 +7,30 @@ using TecnocampusProjectII;
 public abstract class FireActivationObject : MonoBehaviour
 {
     [SerializeField] protected KeyCode _interactKey = KeyCode.E;
-    protected bool _isActivated;
+    protected bool _isActivated = false;
     protected bool _canActivate = false;
+    Thrower _thrower;
+
+    private void Start()
+    {
+        _thrower = FindObjectOfType<Thrower>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //DETECTAR SI EL QUE ENTRA ES PLAYER, TIENE FUEGO
         if (_isActivated) return;
 
-        Thrower thrower = collision.GetComponent<Thrower>();
-        if (thrower == null) return;
-
-        if (thrower.HasFire)
-        {
-            _canActivate = true;
-        }
+        
+        if (_thrower.transform != collision.transform) return;
+        _canActivate = true;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (_isActivated) return;
 
-        Thrower thrower = collision.GetComponent<Thrower>();
-        if (thrower == null) return;
-
-        if (thrower.HasFire)
-        {
-            _canActivate = false;
-        }
+        if (_thrower.transform != collision.transform) return;
+        _canActivate = false;
     }
 
     private void Update()
@@ -41,7 +38,7 @@ public abstract class FireActivationObject : MonoBehaviour
         if (_isActivated)
             return;
 
-        if (_canActivate && Input.GetKeyDown(_interactKey))
+        if (_canActivate && Input.GetKeyDown(_interactKey) && _thrower.HasFire)
         {
             Activate();
         }
