717fc8f067d914e452a4681fbdc3444c3be6b0dc
MarcSans6 <marcsanscormenzana@gmail.com>
2023-05-30 04:00:26 +0200

    camera prefab done

--- Assets/Scripts/Camera/RoomWithCameraBehaviour.cs
+++ Assets/Scripts/Camera/RoomWithCameraBehaviour.cs
@@ -7,26 +7,56 @@ public class RoomWithCameraBehaviour : MonoBehaviour
 {
     [SerializeField]
     private CinemachineVirtualCamera _virtualCamera;
+    public CinemachineVirtualCamera Cam => _virtualCamera;
+    private bool _playerInTrigger = false;
+    public bool PlayerInTrigger => _playerInTrigger;
+    RoomCamManager _roomCamManager;
+    
+    [SerializeField]
+    float _scale;
 
-    CameraManager _camManager;
+    private void OnValidate()
+    {
+        UpdateCamSize();
+    }
+
+    private void UpdateCamSize()
+    {
+        transform.localScale = new Vector2(16, 9) * _scale;
+        _virtualCamera.m_Lens.OrthographicSize = transform.localScale.y / 2;
+    }
 
     private void Awake()
     {
-        //_virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
+        _virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
+        _roomCamManager =  RoomCamManager.GetCameraManager();
+        _roomCamManager.AddToRoomList(this);
     }
 
     private void Start()
     {
-        _camManager =  CameraManager.GetCameraManager();
-        _camManager.AddToCameraList(_virtualCamera);
+    }
+
+    private void Update()
+    {
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            _camManager.SetCurrentCamera(_virtualCamera);
+            _playerInTrigger = true;
+            _roomCamManager.SetCurrentRoomCam(this);
         }
 
+    }
+
+    private void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInTrigger = false;
+        }
     }
 }
