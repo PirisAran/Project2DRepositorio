38b848e16eb4696b505766ce30714e8f9f180691
MarcSans6 <marcsanscormenzana@gmail.com>
2023-06-12 23:37:19 +0200

    Revert "plataformas con collider, nueva gotatrap, y escena boss en proceso"
    This reverts commit 736bd9daa4830850de1e09ba97069c15e01965ed.

--- Assets/Resources/Prefabs/Particles/ParticlePrefabBehaviour.cs
+++ Assets/Resources/Prefabs/Particles/ParticlePrefabBehaviour.cs
@@ -1,26 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TecnocampusProjectII;
-public class ParticlePrefabBehaviour : MonoBehaviour, IRestartLevelElement
+
+public class ParticlePrefabBehaviour : MonoBehaviour
 {
     [SerializeField] float _lifeTime;
     
     // Start is called before the first frame update
     void Start()
     {
-        GameLogic.GetGameLogic().GetGameController().GetLevelController().AddRestartLevelElement(this);
         StartCoroutine(DestroyAfterTime(_lifeTime));
     }
 
     IEnumerator DestroyAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
-        Destroy(gameObject);
-    }
-
-    public void RestartLevel()
-    {
         Destroy(gameObject);
     }
 }
