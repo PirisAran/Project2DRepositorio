86ffc5160250650084d96965a3bc203e212e4ecc
MarcSans6 <marcsanscormenzana@gmail.com>
2023-06-13 20:55:37 +0200

    commit

---
+++ Assets/Scripts/Umbra/UmbraMini.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using TecnocampusProjectII;
+
+public class UmbraMini : MonoBehaviour, IRestartLevelElement
+{
+    PlayerController _player;
+    float _speed;
+    Vector2 _oPosition;
+    private void Awake()
+    {
+        _oPosition = transform.position;
+        gameObject.SetActive(false);
+    }
+    private void Start()
+    {
+        _player = GameLogic.GetGameLogic().GetGameController().m_Player;
+        GameLogic.GetGameLogic().GetGameController().GetLevelController().AddRestartLevelElement(this);
+    }
+
+    public void Activate(float speed)
+    {
+        gameObject.SetActive(true);
+        _speed = speed;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveTowardsPlayer();
+        
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+                                                _player.transform.position,
+                                                _speed * Time.fixedDeltaTime);
+
+    }
+
+    public void RestartLevel()
+    {
+        gameObject.SetActive(false);
+        transform.position = _oPosition;
+    }
+}
