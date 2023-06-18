917ec969e69cf951d0353553bbbdda41f5c7c04d
MarcSans6 <marcsanscormenzana@gmail.com>
2023-06-19 00:38:07 +0200

    11

--- Assets/FinishLevelColliderBehaviour.cs
+++ Assets/FinishLevelColliderBehaviour.cs
@@ -20,16 +20,45 @@ public class FinishLevelColliderBehaviour : MonoBehaviour
         {
             _fireInsideCollider = true;
         }
+
+        if (collision.transform == _player.transform)
+        {
+            _playerInsideCollider = true;
+            if (_fire.IsAttached())
+            {
+                _fireInsideCollider = true;
+            }
+        }
+
+        Debug.Log(_fireInsideCollider + " fire enter");
+        Debug.Log(_playerInsideCollider + " player enter");
     }
+
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.transform == _fire.transform)
+        {
+            _fireInsideCollider = false;
+        }
+
+        if (collision.transform == _player.transform)
+        {
+            _playerInsideCollider = false;
+            if (_fire.IsAttached())
+            {
+                _fireInsideCollider = false;
+            }
+        }
+
+        Debug.Log(_fireInsideCollider + " fire out");
+        Debug.Log(_playerInsideCollider + " player out");
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        CanFinishLevel = false;
         _player = GameLogic.GetGameLogic().GetGameController().m_Player;
         _fire = _player.GetComponentInChildren<FireController>();
     }
