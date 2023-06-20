e1b4be4f75ece6402d40d174683585bfcbddc115
MarcSans6 <marcsanscormenzana@gmail.com>
2023-06-21 01:17:09 +0200

    fire now workd

--- Assets/Resources/Fuego/Scripts/prueba2/ParticlePositionUpdater.cs
+++ Assets/Resources/Fuego/Scripts/prueba2/ParticlePositionUpdater.cs
@@ -16,7 +16,7 @@ public class ParticlePositionUpdater : MonoBehaviour
         Vector3 parentPos = parentTransform.position;
 
         // Calcula la posición simétrica en el eje Y para el objeto hijo
-        Vector3 symPos = new Vector3(parentPos.x, -parentPos.y, parentPos.z);
+        Vector3 symPos = new Vector3(-parentPos.x, -parentPos.y, parentPos.z);
 
         // Actualiza la posición del objeto hijo
         ps.transform.position = symPos;
